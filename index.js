8cffc65968d19bee166d72107fa4e605bf3c225d
hugoiuri <hugoraiuri@gmail.com>
2018-08-06 18:10:00 -0300

    using header Authorization to call

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const request = require('request');
 const username = process.env.BITBUCKET_USER;
 const password = process.env.BITBUCKET_APP_PASSWORD;
 const team = process.env.BITBUCKET_TEAM;
-const baseUrl = 'https://' + username + ':' + password + '@api.bitbucket.org/2.0';
+const auth = "Basic " + new Buffer.from(username + ":" + password).toString("base64");
+
+const baseUrl = 'https://api.bitbucket.org/2.0';
 
 const options = {
-  url: `${baseUrl}/repositories/${team}`
+  url: `${baseUrl}/repositories/${team}`,
+  headers: {
+    Authorization: auth
+  }
 }
 
 request(options, (err, res, body) => {
